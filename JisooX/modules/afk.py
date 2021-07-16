04dc858d37db52e16aa6618cc36c5800a86a55e1
SkyzoGans <81844427+ridho17-ind@users.noreply.github.com>
2021-07-16 18:14:58 +0700

    Update afk.py

--- JisooX/modules/afk.py
+++ JisooX/modules/afk.py
@@ -27,7 +27,7 @@ def afk(bot: Bot, update: Update):
 
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
-    update.effective_message.reply_text("{} is now away!".format(fname))
+    update.effective_message.reply_text("{} Is Now Away, Waiting Until Back!".format(fname))
 
     
 @run_async
