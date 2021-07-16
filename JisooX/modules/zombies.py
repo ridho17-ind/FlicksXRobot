3f95db19288994c75ce9bb78bb9508b82f0816ba
SkyzoGans <81844427+ridho17-ind@users.noreply.github.com>
2021-07-16 16:50:55 +0700

    Create zombies.py

---
+++ JisooX/modules/zombies.py
@@ -0,0 +1,122 @@
+from asyncio import sleep
+
+from telethon import events
+from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights
+
+from JisooX import OWNER_ID
+from JisooX.services.telethon import tbot as client
+
+# =================== CONSTANT ===================
+
+BANNED_RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
+
+
+UNBAN_RIGHTS = ChatBannedRights(
+    until_date=None,
+    send_messages=None,
+    send_media=None,
+    send_stickers=None,
+    send_gifs=None,
+    send_games=None,
+    send_inline=None,
+    embed_links=None,
+)
+
+OFFICERS = OWNER_ID
+
+# Check if user has admin rights
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await tbot.get_peer_id(user)
+        ps = (
+            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
+        ).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return None
+
+
+@client.on(events.NewMessage(pattern=f"^[!/]zombies ?(.*)"))
+async def zombies(event):
+    """For .zombies command, list all the zombies in a chat."""
+    if event.fwd_from:
+        return
+    if event.is_group:
+        if await is_register_admin(event.input_chat, event.message.sender_id):
+            pass
+        else:
+            return
+    con = event.pattern_match.group(1).lower()
+    del_u = 0
+    del_status = "No Deleted Accounts Found, Group Is Clean."
+
+    if con != "clean":
+        find_zombies = await event.respond("Searching For Zombies...")
+        async for user in event.client.iter_participants(event.chat_id):
+
+            if user.deleted:
+                del_u += 1
+                await sleep(1)
+        if del_u > 0:
+            del_status = f"Found **{del_u}** Zombies In This Group.\
+            \nClean Them By Using - `/zombies clean`"
+        await find_zombies.edit(del_status)
+        return
+
+    # Here laying the sanity check
+    chat = await event.get_chat()
+    chat.admin_rights
+    chat.creator
+
+    # Well
+
+    cleaning_zombies = await event.respond("Cleaning Zombies...")
+    del_u = 0
+    del_a = 0
+
+    async for user in event.client.iter_participants(event.chat_id):
+        if user.deleted:
+            try:
+                await event.client(
+                    EditBannedRequest(event.chat_id, user.id, BANNED_RIGHTS)
+                )
+            except ChatAdminRequiredError:
+                await cleaning_zombies.edit("I Don't Have Ban Rights In This Group.")
+                return
+            except UserAdminInvalidError:
+                del_u -= 1
+                del_a += 1
+            await event.client(EditBannedRequest(event.chat_id, user.id, UNBAN_RIGHTS))
+            del_u += 1
+
+    if del_u > 0:
+        del_status = f"Cleaned `{del_u}` Zombies"
+
+    if del_a > 0:
+        del_status = f"Cleaned `{del_u}` Zombies \
+        \n`{del_a}` Zombie Admin Accounts Are Not Removed!"
+
+    await cleaning_zombies.edit(del_status)
