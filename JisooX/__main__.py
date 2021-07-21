528f0c991eb8a2317ecd3b6fb5f6705b65cbdaab
SkyzoGans <81844427+ridho17-ind@users.noreply.github.com>
2021-07-21 18:39:05 +0700

    Update __main__.py

--- JisooX/__main__.py
+++ JisooX/__main__.py
@@ -176,8 +176,8 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="Help",callback_data="help_back"),InlineKeyboardButton(text="Creator",url="https://t.me/Sadroomsinfo/83")]]
-    keyboard += [[InlineKeyboardButton(text="Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="Add Me",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="ʜᴇʟᴘ",callback_data="help_back"),InlineKeyboardButton(text="ᴅᴇᴠᴏʟᴏᴘᴇʀ",url="https://t.me/Sadroomsinfo/83")]]
+    keyboard += [[InlineKeyboardButton(text="ᴄᴏɴɴᴇᴄᴛɪɴɢ", callback_data="main_connect"),InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
