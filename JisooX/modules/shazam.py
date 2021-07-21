4e33674229cbee14abb580615cdc5a92852ebd01
SkyzoGans <81844427+ridho17-ind@users.noreply.github.com>
2021-07-21 18:56:09 +0700

    Update shazam.py

--- JisooX/modules/shazam.py
+++ JisooX/modules/shazam.py
@@ -46,7 +46,7 @@ async def shazamm(client, message):
     messageo = f"""<b>Song Shazamed.</b>
 <b>Song Name : </b>{title}
 <b>Song By : </b>{by}
-<u><b>Identified By @JisooXRobot
+<u><b>Identified By @SadRoomsInfo
 """
     await client.send_photo(message.chat.id, image, messageo, parse_mode="HTML")
     os.remove(downloaded_file_name)
