75d27f2d4e228e118c456cfea1b0b5e1a5a6dc50
SkyzoGans <81844427+ridho17-ind@users.noreply.github.com>
2021-07-27 17:51:27 +0700

    Update logo.py

--- JisooX/modules/logo.py
+++ JisooX/modules/logo.py
@@ -36,11 +36,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByFlicks.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By Zenwisteria")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By Flicks")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @Rorflame, {e}')
+   await event.reply(f'Error Report @XFLSkyzo, {e}')
 
 
 file_help = os.path.basename(__file__)
@@ -51,4 +51,4 @@ file_helpo = file_help.replace("_", " ")
 __help__ = """
  ❍ /logo text :  Create your logo with your name
  """
-__mod_name__ = "Logo"
+__mod_name__ = "LOGO"
